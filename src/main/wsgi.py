cfd4d174f0c1d7eee57c940c566af33f05f497f2
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2022-04-29 09:02:46 +0200

    Fix settings for gunicorn

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'main.settings_prod'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings_prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = get_wsgi_application()
